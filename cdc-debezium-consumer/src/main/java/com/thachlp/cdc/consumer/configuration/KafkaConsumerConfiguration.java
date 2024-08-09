dbd756c536e78e28783201efa272e3f975b2de62
Thach Le <thach.le@linecorp.com>
2024-08-09 16:55:38 +0700

    Update the config that only receive latest event when consumer starts

--- cdc-debezium-consumer/src/main/java/com/thachlp/cdc/consumer/configuration/KafkaConsumerConfiguration.java
+++ cdc-debezium-consumer/src/main/java/com/thachlp/cdc/consumer/configuration/KafkaConsumerConfiguration.java
@@ -25,6 +25,8 @@ public class KafkaConsumerConfiguration {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "coffee_shop");
         return props;
     }
